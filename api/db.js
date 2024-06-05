2e118144f123d1ef8cdac68efdebaffbd6e0b08e
LeonardoH21 <akahoshi21@gmail.com>
2024-06-05 01:46:16 -0300

    ok

--- api/db.js
+++ api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql"
 
 export const db = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "1234",
+    user: "admin",
+    password: "@dm!n",
     database: "crud"
 })
